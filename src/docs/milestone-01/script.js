ef0586f1a985557c43d49ede3599c78104b0297c
Leoc74 <lciccarelli74@gmail.com>
2024-04-02 00:24:55 -0400

    Added basic CSS styling to the webpage.

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -6,10 +6,12 @@ REQUIREMENTS
 - **DOM Access**: Your JavaScript code must interact with the Document Object Model (DOM)
   using methods such as **`document.getElementById()`**. This interaction is fundamental for
   targeting and manipulating specific elements within your document.
+
 - **Dynamic DOM Updates**: The script must be capable of dynamically updating the DOM, either
   by creating new elements (e.g., using **`document.createElement()`**) or modifying existing
   elements through their **`.innerHTML`** property. This capability allows your document to reflect
   changes in real-time, based on user interaction or other events.
+  
 - **Event-Driven Trigger**: The dynamic feature must be activated by an event, which is achieved by
   attaching an event listener to an element using its **`.addEventListener()`** method. This approach
   ensures that your dynamic aspect responds to user actions, such as clicks, mouseovers, or keyboard input,
