3d900b457c564f9d68ba253d746afb26aca37837
Leoc74 <lciccarelli74@gmail.com>
2024-04-18 16:11:49 -0400

    Maybe dropdown labels react when hovered. Added comments to describe event listeners in js

--- src/client/search.js
+++ src/client/search.js
@@ -3,6 +3,9 @@ let sort_button_direction = false;
 
 let search_box = document.getElementById("search-box");
 
+/**
+ * Event Listener for the sort_button. Should sort the outputs by price increasing/decreasing when pressed
+ */
 sort_button.addEventListener("click", function () {
   if (sort_button_direction) {
     this.value = "Sort by Price: ↑";
@@ -12,6 +15,10 @@ sort_button.addEventListener("click", function () {
   sort_button_direction = !sort_button_direction;
 });
 
+/**
+ * Event Listener for when a user tries to search something in the search bar
+ * Nothing implimented yet
+ */
 search_box.addEventListener("keyup", function (key) {
   if (key.key === "Enter") {
     console.log("Enter");
