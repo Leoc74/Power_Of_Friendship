f802b7b3170183899397a7da26715f5f4b95dfc9
Leoc74 <lciccarelli74@gmail.com>
2024-05-09 23:16:43 -0400

    Implemented functionality for sort button on search page. Works properly with dummy data. Works to dynamically sort results when clicked and also to import new results in the specified order

--- src/client/search.js
+++ src/client/search.js
@@ -1,7 +1,7 @@
 import * as db from "./db.js";
 
 let sort_button = document.getElementById("sort-button");
-let sort_button_direction = false;
+let sort_button_increasing = true;
 
 let search_box = document.getElementById("search-box");
 
@@ -16,12 +16,13 @@ window.addEventListener("load", async () => {
  * Event Listener for the sort_button. Should sort the outputs by price increasing/decreasing when pressed
  */
 sort_button.addEventListener("click", function () {
-  if (sort_button_direction) {
+  if (!sort_button_increasing) {
     this.value = "Sort by Price: ↑";
   } else {
     this.value = "Sort by Price: ↓";
   }
-  sort_button_direction = !sort_button_direction;
+  sort_button_increasing = !sort_button_increasing;
+  sortElements(sort_button_increasing);
 });
 
 /**
@@ -33,22 +34,56 @@ search_box.addEventListener("keyup", async function (event) {
   if (event.key === "Enter") {
     results_container.innerHTML = "";
     let results = await db.getAllProducts();
-    for (let result of results.rows) {
-      let title = result.doc.title;
-      let price = result.doc.price;
-      let content = result.doc.content;
-      let imagePath = result.doc.imagePath;
-      let link = result.doc.link;
-      let resultElement = document.createElement("div");
-      resultElement.classList.add("result");
-      resultElement.innerHTML = `
-      <a href="${link}" target="_blank">
-        <h3>${title}</h3>
-      </a>
-      <p>Price: ${price}</p>
-      <p>${content}</p>
-      <img src="${imagePath}" alt="Image not Found">`;
-      results_container.appendChild(resultElement);
-    }
+    loadSearchResults(results, sort_button_increasing);
   }
 });
+
+/**
+ * Adds the results to the DOM on the Search Page in order according to Price
+ * @param {Object} results The parsed results from the web scraping
+ * @param {boolean} price_increasing Specifies true for prices in acsending order, false for descending order
+ */
+function loadSearchResults(results, price_increasing) {
+  results.rows.sort((a, b) => {
+    let x = parseFloat(a.doc.price.substring(1));
+    let y = parseFloat(b.doc.price.substring(1));
+    return price_increasing ? x - y : y - x;
+  });
+  for (let result of results.rows) {
+    let title = result.doc.title;
+    let price = result.doc.price;
+    let content = result.doc.content;
+    let imagePath = result.doc.imagePath;
+    let link = result.doc.link;
+    let resultElement = document.createElement("div");
+    resultElement.classList.add("result");
+    resultElement.innerHTML = `
+    <a href="${link}" target="_blank">
+      <h3>${title}</h3>
+    </a>
+    <p class="price">Price: ${price}</p>
+    <p>${content}</p>
+    <img src="${imagePath}" alt="Image not Found">`;
+    results_container.appendChild(resultElement);
+  }
+}
+
+/**
+ * This function re-orders search results once they have already been populated
+ * @param {boolean} price_increasing Specifies the direction the prices should be sorted.
+ */
+function sortElements(price_increasing) {
+  let results = [];
+  for (let child of results_container.children) {
+    results.push(child);
+  }
+  results.sort((a, b) => {
+    let e1 = parseFloat(a.children[1].innerText.substring(8));
+    let e2 = parseFloat(b.children[1].innerText.substring(8));
+    return price_increasing ? e1 - e2 : e2 - e1;
+  });
+  results_container.innerHTML = "";
+  for (let res of results) {
+    results_container.appendChild(res);
+  }
+}
