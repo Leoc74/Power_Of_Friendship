07ee376d202a138fbb2cc5a872b6a848245d9154
amirag24 <148915113+amirag24@users.noreply.github.com>
2024-05-12 20:34:09 -0400

    Added Khols,asos and northface scraping, it takes the information from the site and returns an array of product information (name, price, image and url)

--- src/server/northface.js
+++ src/server/northface.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch';
 
-async function fetchProductData() {
+async function scrapeNorthFace() {
     try {
         // Fetch data from the asos API endpoint
-        const response = await fetch('https://www.asos.com/api/product/search/v2/categories/13489?offset=72&key-search-mvtid=899035-web-new-in-product-type-mods&store=US&lang=en-US&currency=USD&rowlength=2&channel=mobile-web&country=US&keyStoreDataversion=q1av6t0-39&advertisementsPartnerId=100716&advertisementsVisitorId=97d2b7fe-07f8-44aa-8897-6519242eb4de&advertisementsOptInConsent=true&limit=72&region=NH');
+        const response = await fetch('https://www.thenorthface.com/api/products/v1/catalog?start=24&count=24&sort=bestMatches&locale=en-us&filters=cgid=359774&fqj=%7B%22cgid%22%3A%22359774%22%7D');
         const data = await response.json();
-
+        console.log(data.products[0]);
         // Initialize an array to store product data
         const products = [];
         
@@ -13,14 +13,15 @@ async function fetchProductData() {
         if ( data.products) {
             // Extract data for the first 10 products
             for (let i = 0; i < 10 && i <  data.products.length; i++) {
+                //console.log(data.products[0].productTitle);
                 const product =  data.products[i];
 
                 // Extract product details
-                const imageUrl = product.additionalImageUrls[0];
-                const price = product.price.current.value; 
+                const imageUrl = product.images;
+                const price = product.price.current; 
                 const title = product.name; 
-                const baseURL = 'https://www.asos.com/us';
-                const partialUrl = product.url;
+                const baseURL = 'https://www.https://www.thenorthface.com';
+                const partialUrl = product.slug;
                 const productURL = baseURL + partialUrl;
 
 
@@ -38,16 +39,12 @@ async function fetchProductData() {
         throw new Error('Error fetching product data: ' + error.message);
     }
 }
-
-// Example usage
+// //Example usage
 // fetchProductData()
-//     .then(products => {
-//         // Log the products data (for testing purposes)
-//        console.log(products);
+//    .then(products => {
+//        //console.log(products);
 
-//         // Perform further processing or display on website
 //     })
 //     .catch(error => {
-//         // Log any errors that occur during the fetching process
-//         console.error(error.message);
+//        console.error(error.message);
 //     });
