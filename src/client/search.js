d29ddfcdf0b827cdeef62b23f38952a2e81be7b4
Leoc74 <lciccarelli74@gmail.com>
2024-05-14 00:10:52 -0400

    The server now pulls from northface data when the event listener is triggered in the search bar. Made the sorting work with new format. Commented out outdated image carousel code in script.js

--- src/client/search.js
+++ src/client/search.js
@@ -10,10 +10,14 @@ let results_container = document.getElementById("results-container");
 
 const URL = "http://127.0.0.1:3000"; // URL of our server
 
-window.addEventListener("load", async () => {
-  //await db.printData();
-  await db.initializeDataBase();
-});
+/**
+ * Loads the dummy data
+ * NOT Implimented anymore
+ */
+// window.addEventListener("load", async () => {
+//   //await db.printData();
+//   await db.initializeDataBase();
+// });
 
 /**
  * Event Listener for the sort_button. Should sort the outputs by price increasing/decreasing when pressed
@@ -40,12 +44,13 @@ search_box.addEventListener("keyup", async function (event) {
     //DUMMY DATA:
     //let results = await db.getAllProducts();
     let searchText = search_box.value;
-    //TODO Doesn't work yet
     let results = await fetch(`${URL}/search?searchText=${searchText}`, {
       method: "GET",
     });
-    //console.log(await results.json());
-    loadSearchResults(results, sort_button_increasing);
+    let products = JSON.parse(await results.text());
+    console.log(results);
+    console.log(products);
+    loadSearchResults(products, sort_button_increasing);
   }
 });
 
@@ -55,31 +60,32 @@ search_box.addEventListener("keyup", async function (event) {
  * @param {boolean} price_increasing Specifies true for prices in acsending order, false for descending order
  */
 function loadSearchResults(results, price_increasing) {
-  results.rows.sort((a, b) => {
-    let x = parseFloat(a.doc.price.substring(1)); // Change to not use substring for real data
-    let y = parseFloat(b.doc.price.substring(1));
+  results.sort((a, b) => {
+    let x = parseFloat(a.price); // Change to not use substring for real data
+    let y = parseFloat(b.price);
     return price_increasing ? x - y : y - x;
   });
-  for (let result of results.rows) {
-    let title = result.doc.title;
-    let price = result.doc.price;
-    let content = result.doc.content;
-    let imagePath = result.doc.imagePath;
-    let link = result.doc.link;
-    /*
+  for (let result of results) {
+    // let title = result.doc.title;
+    // let price = result.doc.price;
+    // let content = result.doc.content;
+    // let imagePath = result.doc.imagePath;
+    // let link = result.doc.link;
     let title = result.title;
     let price = result.price;
-    let content = not implimented yet;
+    let content = "Not implimented yet";
     let imagePath = result.imageUrl;
-    let link = result.productUrl;
-    */
+    let link = result.productURL;
+
+    console.log(`Link: ${link}`);
+
     let resultElement = document.createElement("div");
     resultElement.classList.add("result");
     resultElement.innerHTML = `
     <a href="${link}" target="_blank">
       <h3>${title}</h3>
     </a>
-    <p class="price">Price: ${price}</p>
+    <p class="price">Price: $${price}</p>
     <p>${content}</p>
     <img src="${imagePath}" alt="Image not Found">`;
     results_container.appendChild(resultElement);
@@ -96,8 +102,8 @@ function sortElements(price_increasing) {
     results.push(child);
   }
   results.sort((a, b) => {
-    let e1 = parseFloat(a.children[1].innerText.substring(8)); //TODO change to 7 since $ is not in price anymore
-    let e2 = parseFloat(b.children[1].innerText.substring(8));
+    let e1 = parseFloat(a.children[1].innerText.substring(7)); //TODO change to 7 since $ is not in price anymore
+    let e2 = parseFloat(b.children[1].innerText.substring(7));
     return price_increasing ? e1 - e2 : e2 - e1;
   });
   results_container.innerHTML = "";
